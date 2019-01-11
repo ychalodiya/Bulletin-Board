a80b20dd97fe021994a62ea09d0e7dfd77cbd187
Yogesh Chalodiya <yogeshchalodiya@Yogeshs-MacBook-Pro.local>
2019-01-10 19:07:55 -0500

    Uploading working projects of Bulletin Board - Check it out

---
+++ precache-manifest.3c4ee6113dd7eb04dcd7aa35b2d10b94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "657547f790b8c3720984",
+    "url": "/static/css/main.eb9e905c.chunk.css"
+  },
+  {
+    "revision": "657547f790b8c3720984",
+    "url": "/static/js/main.657547f7.chunk.js"
+  },
+  {
+    "revision": "0460dd06d744a15c4a94",
+    "url": "/static/js/1.0460dd06.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "564c24ec28140a9c2318c481964787c0",
+    "url": "/index.html"
+  }
+];
